a54334cf231da916c7a7394a8cd38da206e23cb8
hacon1502 <hacon150296@gmail.com>
2017-05-28 17:23:01 +0700

    Quan Li Kho

---
+++ DAL/HangHoa_DAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using DTO;
+
+namespace DAL
+{
+    public class HangHoa_DAL : DatabaseAccess2
+    { 
+        public List<HangHoa_DTO> ThongTinHangHoa()
+        {
+           
+        }
+    }
+}
---
+++ DAL/PhieuXuat_DAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using DTO;
+
+namespace DAL
+{
+    public class PhieuXuat_DAL : DatabaseAccess2
+    {
+        
+
+    }
+}
