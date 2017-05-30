373670e2d230323852054118ee6ce8c3afc959cb
ndhuy96 <ndhuy96@gmail.com>
2017-05-30 17:59:24 +0700

    UpdateNhapXuat

--- BLL/ChiTietPX_BLL.cs
+++ BLL/ChiTietPX_BLL.cs
@@ -10,6 +10,22 @@ namespace BLL
 {
     public class ChiTietPX_BLL
     {
-        
+        ChiTietPX_DAL ctpxDAL = new ChiTietPX_DAL();
+        public List<ChiTietPX_DTO> ChiTietPX(string mapx)
+        {
+            return ctpxDAL.ChiTietPX(mapx);
+        }
+        public bool ThemChiTietPX(ChiTietPX_DTO ctpx)
+        {
+            return ctpxDAL.ThemChiTietPX(ctpx);
+        }
+        public bool SuaChiTietPX(ChiTietPX_DTO ctpx)
+        {
+            return ctpxDAL.SuaChiTietPX(ctpx);
+        }
+        public bool XoaChiTietPX(string MaHang)
+        {
+            return ctpxDAL.XoaChiTietPN(MaHang);
+        }
     }
 }
--- BLL/PhieuXuat_BLL.cs
+++ BLL/PhieuXuat_BLL.cs
@@ -10,6 +10,32 @@ namespace BLL
 {
     public class PhieuXuat_BLL
     {
-        
+        PhieuXuat_DAL pxDAL = new PhieuXuat_DAL();
+
+        public List<PhieuXuat_DTO> ThongTinPhieuXuat()
+        {
+            return pxDAL.ThongTinPhieuXuat();
+        }
+        public List<HangHoa_DTO> autoDuLieu()
+        {
+            return pxDAL.autoDuLieu();
+        }
+        public bool ThemPhieuXuat(PhieuXuat_DTO px)
+        {
+            return pxDAL.ThemPhieuXuat(px);
+        }
+        public bool SuaPhieuXuat(PhieuXuat_DTO px)
+        {
+            return pxDAL.SuaPhieuXuat(px);
+        }
+        public bool XoaPhieuXuat(string MaPX)
+        {
+            return pxDAL.XoaPhieuXuat(MaPX);
+        }
+        public List<PhieuXuat_DTO> TimKiemPX(string mapx)
+        {
+            return pxDAL.TimKiemPX(mapx);
+        }
+
     }
 }
